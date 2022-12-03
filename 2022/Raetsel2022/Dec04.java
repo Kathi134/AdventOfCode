d914afc1b520f68bae74e1c4d25017555c024130
Katharina Reiner <katharina.reiner@hof-university.de>
2022-12-03 08:22:52 +0100

    Dec 4 skeleton

---
+++ 2022/Raetsel2022/Dec04.java
@@ -0,0 +1,34 @@
+package Raetsel2022;
+import Base.InputKind;
+
+public class Dec04 extends Puzzle2022
+{
+	public Dec04()
+	{
+		super();
+		readInput(InputKind.);
+		
+		computeSolution(1);
+		computeSolution(2);
+
+		printSolution();
+	}
+
+	@Override
+	public void solveTask1()
+	{
+		
+	}
+
+	@Override
+	public void solveTask2()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		new Dec04();
+	}
+}
+
