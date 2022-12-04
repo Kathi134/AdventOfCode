cdc19e82301b4a5a86647267e878865180328da4
Katharina Reiner <katharina.reiner@hof-university.de>
2022-12-04 07:23:27 +0100

    day 5 base

---
+++ 2022/Raetsel2022/Dec05.java
@@ -0,0 +1,36 @@
+package Raetsel2022;
+
+import Base.InputKind;
+
+public class Dec05 extends Puzzle2022
+{
+	public Dec05()
+	{
+		super(5);
+		readInput(InputKind.);
+		
+		computeSolution(1);
+		computeSolution(2);
+
+		printSolution();
+	}
+	
+	@Override
+	public void solveTask1()
+	{
+		
+	}
+	
+
+	@Override
+	public void solveTask2()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		new Dec04();
+	}
+}
+
